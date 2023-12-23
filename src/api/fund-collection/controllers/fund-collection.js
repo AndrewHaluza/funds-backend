41d254145ec89d792f768c5b320d8a705410df6f
ND <nd@newerest.space>
2023-12-23 13:10:05 +0200

    Add new component and collection types

---
+++ src/api/fund-collection/controllers/fund-collection.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * fund-collection controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::fund-collection.fund-collection');
---
+++ src/api/fund-collection/services/fund-collection.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * fund-collection service
+ */
+
+const { createCoreService } = require('@strapi/strapi').factories;
+
+module.exports = createCoreService('api::fund-collection.fund-collection');
